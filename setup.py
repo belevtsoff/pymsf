087822cc7f22dc4b3b99179d4eb5a80ad3c7a32d
belevtsoff <belevtsoff@gmail.com>
2013-06-01 00:21:44 +0200

    added lapacke to dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import Cython.Compiler.Options as o
+
+o.annotate = True
 
 ext_modules = [
-               Extension("integrator", ["src/integrator.pyx"], include_dirs=['src/models']),
-               Extension("msf_tools", ["src/msf_tools.pyx"], include_dirs=['src/models'], libraries=['cblas']),
+               Extension("integrator", ["src/integrator.pyx"]),
+               Extension("msf_tools", ["src/msf_tools.pyx"],
+                   libraries=['atlas', 'lapacke'],
+                   include_dirs=['src/models']),
                Extension("base_model", ["src/models/base_model.pyx"]),
                Extension("sgy_neuron", ["src/models/sgy_neuron.pyx"]),
                Extension("sgy_double_neuron", ["src/models/sgy_double_neuron.pyx"])
                ]
 
 setup(
-  name = 'Limit cycle search',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules,
+  name='Limit cycle search',
+  cmdclass={'build_ext': build_ext},
+  ext_modules=ext_modules
 )
 
